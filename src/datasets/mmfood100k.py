3f613c3556b9db79ccffd1d65b9fcdfdf6bbda62
LuckyToaster <senchymarques@hotmail.com>
2026-01-31 20:30:27 +0100

    improved the dataset downloading logic

--- src/datasets/mmfood100k.py
+++ src/datasets/mmfood100k.py
@@ -14,9 +14,9 @@ class MMFood100K:
         self.csv_path = self.dir / 'mm-food-100k.csv'
         self.imgs_dir = self.dir / 'imgs'
         self.dir.mkdir(parents=True, exist_ok=True)
-        self.df = pd.read_csv(self.csv_path) if self.csv_path.is_file() else self.__download()
+        self.df = pd.read_csv(self.csv_path) if self.csv_path.is_file() else self.__download_csv()
 
-    def __download(self):
+    def __download_csv(self):
         print(f'{str(self.csv_path)} not present, downloading ...')
         df = pd.read_csv(self.url)
         df = df.rename(columns={'image_url': 'img_url'})
@@ -26,23 +26,61 @@ class MMFood100K:
         df.to_csv(self.csv_path, index=False)
         return df
 
+    def __get_missing_imgs_ids(self):
+        if not self.imgs_dir.exists() or not any(self.imgs_dir.iterdir()): 
+            return sorted(range(0, 100_000))
+        ids_present = [int(i.name.split('.')[0]) for i in self.imgs_dir.iterdir()]
+        return sorted(set(range(0, 100_000)) - set(ids_present))
+
     @staticmethod
     async def __throttle(semaphore, session, url, path):
         async with semaphore:
             return await download_img(session, url, path)
 
-    async def download_imgs(self, limit=20):
-        if self.imgs_dir.exists() and any(self.imgs_dir.iterdir()): 
-            return self
+    async def download_imgs(self, limit=25):
+        self.imgs_dir.mkdir(parents=True, exist_ok=True)
 
-        print(f'{str(self.imgs_dir)} not present, downloading ...')
-        self.imgs_dir.mkdir(parents=True)
-        urls = self.df['img_url'].tolist()
-        paths = self.df['img_path'].tolist()
+        while True:
+            ids_missing = self.__get_missing_imgs_ids()
+            if not ids_missing:
+                print(f'{self.imgs_dir} finished downloading')
+                break
 
-        async with ClientSession() as s:         
-            sem = Semaphore(limit)
-            tasks = [create_task(self.__throttle(sem, s, u, p)) for u, p in zip(urls, paths)]
-            await gather(*tasks)
+            print(f'Downloading {len(ids_missing)} missing images')
+            urls = self.df.iloc[ids_missing]['img_url']
+            paths = self.df.iloc[ids_missing]['img_path']
+            async with ClientSession() as s:         
+                sem = Semaphore(limit)
+                tasks = [create_task(self.__throttle(sem, s, u, p)) for u, p in zip(urls, paths)]
+                await gather(*tasks)
 
-        return self
+        # if self.imgs_dir.exists() and any(self.imgs_dir.iterdir()): 
+        #     return self
+
+        # if self.imgs_dir.exists():
+        #     ids_present = [int(i.name.split('.')[0]) for i in self.imgs_dir.iterdir()]
+        #     ids_missing = sorted(set(range(0, 100_000)) - set(ids_present))
+        #
+        #     urls = self.df.iloc[ids_missing]['img_url']
+        #     paths = self.df.iloc[ids_missing]['img_path']
+        #
+        #     async with ClientSession() as s:         
+        #         sem = Semaphore(limit)
+        #         tasks = [create_task(self.__throttle(sem, s, u, p)) for u, p in zip(urls, paths)]
+        #         await gather(*tasks)
+        #
+        # else:
+        #     print(f'{str(self.imgs_dir)} not present, downloading ...')
+        #     self.imgs_dir.mkdir(parents=True)
+        #
+        #     urls = self.df['img_url'].tolist()
+        #     paths = self.df['img_path'].tolist()
+        #
+        #     async with ClientSession() as s:         
+        #         sem = Semaphore(limit)
+        #         tasks = [create_task(self.__throttle(sem, s, u, p)) for u, p in zip(urls, paths)]
+        #         await gather(*tasks)
+        #
+        #     return self
+
+
