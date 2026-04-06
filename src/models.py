09ddcaedac2aa12836fbae8962462858b6c52aa5
LuckyToaster <senchymarques@hotmail.com>
2026-04-06 19:14:07 +0200

    added feature_extraction flag to functions in src/models.py

--- src/models.py
+++ src/models.py
@@ -3,26 +3,23 @@ from torch.nn import Linear
 from torchvision.models import \
     efficientnet_b3, EfficientNet_B3_Weights, \
     efficientnet_v2_s, EfficientNet_V2_S_Weights, \
-    efficientnet_v2_m, EfficientNet_V2_M_Weights, \
-    efficientnet_v2_l, EfficientNet_V2_L_Weights, \
     mobilenet_v3_large, MobileNet_V3_Large_Weights, \
     shufflenet_v2_x2_0, ShuffleNet_V2_X2_0_Weights, \
-    swin_v2_t, Swin_V2_T_Weights, \
-    swin_v2_s, Swin_V2_S_Weights, \
-    swin_v2_b, Swin_V2_B_Weights \
+    swin_v2_s, Swin_V2_S_Weights
 
-__all__ = [
-    'get_EfficientNet_B3', 'get_EfficientNet_V2_S', 'get_EfficientNet_V2_M', 'get_EfficientNet_V2_L',
-    'get_MobileNet_V3_L', 
-    'get_ShuffleNet_V2_X2_0',
-    'get_Swin_V2_T', 'get_Swin_V2_S', 'get_Swin_V2_B'
-]
+__all__ = ['get_EfficientNet_B3', 'get_EfficientNet_V2_S', 'get_MobileNet_V3_L', 'get_ShuffleNet_V2_X2_0', 'get_Swin_V2_S']
 
 
-def get_EfficientNet_B3(verbose=True):
+def _freeze(model):
+    for param in model.parameters():
+        param.requires_grad = False
+
+
+def get_EfficientNet_B3(feature_extraction=False, verbose=True):
     weights = EfficientNet_B3_Weights.DEFAULT
     model = efficientnet_b3(weights=weights)
     preprocess = weights.transforms()
+    if feature_extraction: _freeze(model)
     # adapt the head for regression
     in_features = model.classifier[1].in_features
     model.classifier[1] = Linear(in_features, 3)
@@ -31,10 +28,11 @@ def get_EfficientNet_B3(verbose=True):
     return model, preprocess
 
 
-def get_EfficientNet_V2_S(verbose=True):
+def get_EfficientNet_V2_S(feature_extraction=False, verbose=True):
     weights = EfficientNet_V2_S_Weights.DEFAULT
     model = efficientnet_v2_s(weights=weights)
     preprocess = weights.transforms()
+    if feature_extraction: _freeze(model)
     # adapt the head for regression
     in_features = model.classifier[1].in_features
     model.classifier[1] = Linear(in_features, 3)
@@ -43,22 +41,11 @@ def get_EfficientNet_V2_S(verbose=True):
     return model, preprocess
 
 
-def get_EfficientNet_V2_M(verbose=True):
-    weights = EfficientNet_V2_M_Weights.DEFAULT
-    model = efficientnet_v2_m(weights=weights)
-    preprocess = weights.transforms()
-    # adapt the head for regression
-    in_features = model.classifier[1].in_features
-    model.classifier[1] = Linear(in_features, 3)
-    xavier_uniform_(model.classifier[1].weight)
-    if verbose: print(f'EfficientNet V2 Medium: {preprocess}')
-    return model, preprocess
-
-
-def get_MobileNet_V3_L(verbose=True):
+def get_MobileNet_V3_L(feature_extraction=False, verbose=True):
     weights = MobileNet_V3_Large_Weights.DEFAULT
     preprocess = weights.transforms()
     model = mobilenet_v3_large(weights=weights)
+    if feature_extraction: _freeze(model)
     # adapt the head for regression
     last_channel = model.classifier[3].in_features
     model.classifier[3] = Linear(last_channel, 3)
@@ -66,42 +53,45 @@ def get_MobileNet_V3_L(verbose=True):
     return model, preprocess
 
 
-def get_ShuffleNet_V2_X2_0(verbose=True):
-    # https://github.com/pytorch/vision/blob/main/torchvision/models/shufflenetv2.py#L193
+def get_ShuffleNet_V2_X2_0(feature_extraction=False, verbose=True):
     weights = ShuffleNet_V2_X2_0_Weights.DEFAULT
     preprocess = weights.transforms()
-    model = shufflenet_v2_x2_0(weights=weights, num_classes=3)
-    if verbose: print(f'ShuffleNet V2 2: {preprocess}')
+    model = shufflenet_v2_x2_0(weights=weights)
+    if feature_extraction: _freeze(model)
+    # adapt the head for regression
+    model.fc = Linear(model.fc.in_features, 3)
+    if verbose: print(f'ShuffleNet V2 2.0: {preprocess}')
     return model, preprocess
 
 
-def get_Swin_V2_T(verbose=True):
-    # https://github.com/pytorch/vision/blob/main/torchvision/models/swin_transformer.py
+def get_Swin_V2_T(feature_extraction=False, verbose=True):
     weights = Swin_V2_T_Weights.DEFAULT
     preprocess = weights.transforms()
     model = swin_v2_t(weights=weights)
+    if feature_extraction: _freeze(model)
     # modify the head
     model.head = Linear(model.head.in_features, 3)
     if verbose: print(f'Swin V2 Tiny: {preprocess}')
     return model, preprocess
 
 
-def get_Swin_V2_S(verbose=True):
+def get_Swin_V2_S(feature_extraction=False, verbose=True):
     weights = Swin_V2_S_Weights.DEFAULT
     preprocess = weights.transforms()
     model = swin_v2_s(weights=weights)
+    if feature_extraction: _freeze(model)
     # modify the head
     model.head = Linear(model.head.in_features, 3)
-    if verbose: print(f'Swin V2 Tiny: {preprocess}')
+    if verbose: print(f'Swin V2 Small: {preprocess}')
     return model, preprocess
 
 
-def get_Swin_V2_B(verbose=True):
+def get_Swin_V2_B(feature_extraction=False, verbose=True):
     weights = Swin_V2_B_Weights.DEFAULT
     preprocess = weights.transforms()
     model = swin_v2_b(weights=weights)
+    if feature_extraction: _freeze(model)
     # modify the head
     model.head = Linear(model.head.in_features, 3)
-    if verbose: print(f'Swin V2 Tiny: {preprocess}')
+    if verbose: print(f'Swin V2 Base: {preprocess}')
     return model, preprocess
-
--- src/scripts/train_crossval.py
+++
@@ -1,8 +0,0 @@
-from sklearn.model_selection import KFold
-import pandas as pd
-from src.mmfood100k.dataset import MMFood100KDataset
-
-df = pd.read_csv('data/mm-food-100k/mm-food-100k.csv')
-ds = MMFood100KDataset(df)
-
-
